d84c122d5a7ba6022499b4688430faee283d8349
Lamees <Lamees1harbi@gmail.com>
2025-10-19 16:36:28 +0300

    01

--- Learning/ContentView.swift
+++ Learning/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color("BackgoundColor").edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("").statusBarHidden(false)
+            }
         }
-        .padding()
     }
 }
 
